af8653dbe0f17e9c1e4085c7aa24685295e41315
Goytai <dev.goytai@gmail.com>
2020-08-06 20:10:48 -0300

    Commit Server

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import routes from './routes'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(routes)
+
+// Escuta a porta 3333
+app.listen(3333)
